4e51b2386daf73c2f8b66449b4d0871b74664123
Darshan Pradhan <darshan.pradhan@anu.edu.au>
2022-10-13 10:12:59 +1100

    Module 4

--- sensor/sensor_app.py
+++ sensor/sensor_app.py
@@ -1,9 +1,11 @@
+import re
 from load_data import load_sensor_data
 from house_info import HouseInfo
 from datetime import date, datetime
 from temperature_info import TemperatureData
 from humidity_info import HumidityData
 from statistics import mean
+from particle_count_info import ParticleData
 
 ##############################
 # Do not remove these two lines
@@ -52,5 +54,13 @@ print("\tAverrage: {} humidity".format(mean(recs)))
 
 # Module 5 code here:
 
+particle_data = ParticleData(data)
+recs = particle_data.get_data_by_area(rec_area=test_area)
+print("\nHouse Particle sensor records for area {} = {}".format(test_area, len(recs)))
+
+concentrations = particle_data.get_data_concentrations(data=recs)
+print("\tGood Air Quality Recs: {}".format(concentrations["good"]))
+print("\tModerate Air Quality Recs: {}".format(concentrations["moderate"]))
+print("\tBad Air Quality Recs: {}".format(concentrations["bad"]))
 
 
